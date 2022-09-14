9261c448a10102c0b4994294f3bf2dcdc68ae7f4
WASIFF <90121738+designedbywasiff@users.noreply.github.com>
2022-09-14 10:57:35 +0500

    Create mountains.c

---
+++ mountains.c
@@ -0,0 +1,54 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i=1;i<=10;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=5;i>=1;--i){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=7;i++){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(i=13;i>=1;--i){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=20;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=11;i>=1;--i){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=7;i++){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(i=12;i>=1;--i){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
